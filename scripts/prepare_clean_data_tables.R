11b3e20791a4765aa339340042bd97179530a08c
alvizek <avizek@flowwest.com>
2024-07-08 23:12:08 -0700

    edits to methods

--- scripts/prepare_clean_data_tables.R
+++ scripts/prepare_clean_data_tables.R
@@ -497,51 +497,12 @@ max(uwmp_drought_risk_clean$benefit_demand_reduction_acre_feet, na.rm = T)
 
 write_csv(uwmp_drought_risk_clean, "data/five_year_outlook.csv")
 
-# Monthly CR --------------------------------------------------------------
-# Data:
-# https://data.ca.gov/dataset/drinking-water-public-water-system-operations-monthly-water-production-and-conservation-information
 
-# TODO requires cleaning up the shortage stage. water_shortage_stage is a mess, dwr_shortage_stage only exists for 2022-2023
-cr_raw <- read_csv("data-raw/monthly_CR.csv")
-
-cr_format <- cr_raw |> 
-  select(public_water_system_id, reporting_month, water_shortage_contingency_stage_invoked, 
-         water_shortage_level_indicator, dwr_state_standard_level_corresponding_to_stage) |> 
-  rename(pwsid = public_water_system_id,
-         water_shortage_stage = water_shortage_contingency_stage_invoked,
-         shortage_greater_10_percent = water_shortage_level_indicator,
-         dwr_water_shortage_stage = dwr_state_standard_level_corresponding_to_stage) |> 
-  mutate(month = month(reporting_month, label = T),
-         year = year(reporting_month),
-         shortage_greater_10_percent = case_when(shortage_greater_10_percent == "Yes" ~ T,
-                                                 shortage_greater_10_percent == "No" ~ F),
-         water_shortage_stage = tolower(water_shortage_stage)) |> 
-  select(-reporting_month) |> 
-  select(pwsid, year, month, water_shortage_stage, dwr_water_shortage_stage, shortage_greater_10_percent)
-
-unique(cr_format$water_shortage_stage)
-unique(cr_format$dwr_water_shortage_stage)
-
-write_csv(cr_format, "data/water_shortage_level_clean.csv")
 
 # eAR number of sources ---------------------------------------------------------------------
 # Data: https://www.waterboards.ca.gov/drinking_water/certlic/drinkingwater/ear.html
 
-# TODO need to reformat the source names - get feedback from group whether want to invest
-# time in that
-
-# ear_sources_name <- ear_2022_raw |> 
-#   filter(QuestionName %in% c("SourcesGWGrid", "SourcesGWNotListedGrid", "SourcesSWGrid",
-#                              "SourcesSWNotListedGrid")) |> 
-#   select(WSID, QuestionName, QuestionResults) |> 
-#   rename(pwsid = WSID,
-#          source_type = QuestionName,
-#          source_name = QuestionResults) |> 
-#   mutate(
-#          # reporting year but need to deal with handling of this so it is not manual
-#          year = 2022) |> 
-#   select(pwsid, year, source_type, source_name)
-# write_csv(ear_sources_name, "data/source_name_clean.csv")
+# TODO decide if this is redundant of the source_name table
 
 # Create number of sources table
 ear_tibble <- tibble(years = c(2022:2013),
@@ -690,48 +651,3 @@ dput(unique(source_name$sdwis_water_type)) #c("Groundwater", "Surface Water", "G
 
 write_csv(source_name, "data/source_name.csv")
 
-# SAFER on open data ------------------------------------------------------
-# Data:
-# https://data.ca.gov/dataset/safer-failing-and-at-risk-drinking-water-systems
-
-dw_risk_raw <- read_csv("data-raw/Drinking_Water_Risk_Assessment.csv")
-
-dw_population <- dw_risk_raw |> 
-  select(WATER_SYSTEM_NUMBER, POPULATION) |> 
-  rename(pwsid = WATER_SYSTEM_NUMBER,
-         population = POPULATION)
-write_csv(dw_population, "data/population_clean.csv")
-
-
-# supply and demand -------------------------------------------------------
-
-# These data are published here: https://data.ca.gov/dataset/urws-conservation-supply-demand
-# Represents Monthly CR (2014-2023) and currently reported on SAFER (2023-present)
-
-supply_demand_query <- paste0("https://data.ca.gov/api/3/action/",
-                          "datastore_search_sql?",
-                          "sql=",
-                          URLencode("SELECT * from \"f4d50112-5fb5-4066-b45c-44696b10a49e\""))
-
-supply_demand_list_sql <- fromJSON(supply_demand_query)
-supply_demand_raw_data <- supply_demand_list_sql$result$records
-
-water_shortage <- supply_demand_raw_data |> 
-  select(ORG_ID, WATER_SYSTEM_ID, SUPPLIER_NAME, REPORT_PERIOD_START_DATE, REPORT_PERIOD_END_DATE, DWR_STANDARD_LEVEL) |> 
-  rename(org_id = ORG_ID,
-         pwsid = WATER_SYSTEM_ID,
-         supplier_name = SUPPLIER_NAME,
-         start_date = REPORT_PERIOD_START_DATE,
-         end_date = REPORT_PERIOD_END_DATE,
-         water_shortage_level = DWR_STANDARD_LEVEL) |> 
-  mutate(supplier_name = tolower(supplier_name),
-         # clean up levels, assume if multiple listed than the higher level should be applied
-         water_shortage_level = case_when(water_shortage_level == "1 (Less than 10% Shortage), 2 (10-19% Shortage)" ~ "2 (10-19% Shortage)",
-                                          water_shortage_level == "2 (10-19% Shortage), Not Applicable" ~ "2 (10-19% Shortage)",
-                                          water_shortage_level == "0 (No Shortage Level Invoked), 2 (10-19% Shortage)" ~ "2 (10-19% Shortage)",
-                                          water_shortage_level == "Not Applicable" ~ NA,
-                                          water_shortage_level == "WSCP Does Not Include Stages" ~ NA,
-                                          T ~ water_shortage_level)) |> 
-  # levels were applied beginning in 2022
-  filter(year(start_date) > 2021)
-write_csv(water_shortage, "data/water_shortage.csv")
